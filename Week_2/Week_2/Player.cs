12c13f577390a0dd5ad390fdc65682655aa56282
Sueda Turgut <suedat24@gmail.com>
2023-11-18 12:58:28 +0300

    moving from functional to object oriented

---
+++ Week_2/Week_2/Player.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace Week_2
+{
+	//All class names should start with upper case
+	internal class Player
+	{
+        /*public float x;
+		public float y;
+		public float z;*/
+
+        //public variable names start with Uppercase
+        //private variables start with Lowercase
+
+        private float x;
+		private float y;
+		private float z;
+
+		public static int playerQty;
+        //stored in static memory
+
+        public Player() //Constructor
+                        //Constructour name should always be the same name with class
+        {
+            this.x = 7.0f;
+			this.y = 7.0f;
+			this.z = 7.0f;
+			Player.playerQty += 1;
+            Console.WriteLine("Default constructor");
+        }
+		public Player(float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = 10.0f;
+            Console.WriteLine("Constructor with two arguments");
+        }
+
+        public Player(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            Player.playerQty += 1;
+            Console.WriteLine("Constructor with three arguments");
+        }
+        public Player(float x, float y, float z, string message)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+			Player.playerQty += 1;
+			Console.WriteLine("Constructor with four arguments");
+			Console.WriteLine(message);
+        }
+
+        public void Report()
+		{
+			Console.WriteLine($"Cordinates of the player:");
+            Console.WriteLine($"x: {this.x}");
+            Console.WriteLine($"y: {this.y}");
+            Console.WriteLine($"z: {this.z}");
+        }
+
+		public float GetX()
+		{
+			return this.x;
+		}
+
+        public float GetY()
+        {
+            return this.y;
+        }
+
+        public float GetZ()
+        {
+            return this.z;
+        }
+
+		public void Deconstruct(out float x, out float y, out float z)
+		{
+			x = this.x;
+			y = this.y;
+			z = this.z;
+		}
+ 
+        public static void FirstStaticMethod() //Static method
+        {
+            Console.WriteLine("Hello from static method");
+        }
+
+    }
+}
+
---
+++ Week_2/Week_2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Week_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region first_part
+        /*Player player1 = new Player();
+        player1.x = 1.0f; 
+        player1.y = 2.0f;
+        player1.z = 3.0f;
+
+        Player player2 = new Player();
+        player2.x = 2.0f;
+        player2.y = 1.0f;
+        player2.z = 5.0f;
+
+        int x = 42;
+        DefaultValues myDefaultValues = new DefaultValues();
+        Console.WriteLine($"Default integer; {myDefaultValues.anInt}");
+        Console.WriteLine($"Default float; {myDefaultValues.aFloat}");
+        Console.WriteLine($"Default bool; {myDefaultValues.aBool}");
+        Console.WriteLine($"Default string; {myDefaultValues.aString}");
+        Console.WriteLine($"Int default {default(int)}");
+        Console.WriteLine($"Char default {default(char)}");*/
+        #endregion
+
+        #region second_part
+        /*Player player3 = new Player();
+        Console.WriteLine($"player3 x: {player3.x}");
+        Console.WriteLine($"player3 y: {player3.y}");
+        Console.WriteLine($"player3 z: {player3.z}");
+
+        Player player4 = new Player(1.0f, 2.0f, 3.0f);
+        Console.WriteLine($"player4 x: {player4.x}");
+        Console.WriteLine($"player4 y: {player4.y}");
+        Console.WriteLine($"player4 z: {player4.z}");
+
+        Player player5 = new Player(1.0f, 2.0f, 3.0f);
+        player5.Report();
+
+        float x = player5.GetX();
+        float y = player5.GetY();*/
+        #endregion
+
+        //Static members
+
+        Player player10 = new Player();
+        Console.WriteLine($"Player quantity: {Player.playerQty}");
+        Console.WriteLine("------");
+        Player player11 = new Player();
+        Console.WriteLine($"Player quantity: {Player.playerQty}");
+
+        //Anonymous class
+        var myAnonymousClass = new { name = "sueda", surname = "turgut" };
+        Console.WriteLine($"Name: {myAnonymousClass.name}");
+        Console.WriteLine($"Surname: {myAnonymousClass.surname}");
+
+        Player player20 = new Player();
+        Player player21 = new Player(1.0f, 2.0f);
+        Player player22 = new Player(1.0f, 2.0f, 3.0f);
+        Player player23 = new Player(1.0f, 2.0f, 3.0f, "Not used in the object");
+        Console.ReadLine();
+    }
+}
